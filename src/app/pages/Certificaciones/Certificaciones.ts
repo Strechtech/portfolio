1bfb02c7008cdc5ef86c06884764813845ca00c5
Strechtech <jpachacamasimbana@gmail.com>
2026-06-24 17:29:55 -0500

    fix: agregar comentarios descriptivos a los componentes About y Certificaciones

--- src/app/pages/Certificaciones/Certificaciones.ts
+++ src/app/pages/Certificaciones/Certificaciones.ts
@@ -1,3 +1,4 @@
+// COMPONENTE ENCARGADO DE MOSTRAR LA LISTA DE CERTIFICACIONES
 import { Component } from '@angular/core';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
